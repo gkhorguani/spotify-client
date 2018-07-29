b24fda25e6006973aea82dd487b4f7a3cbec09b5
Giorgi Khorguani <gkhorguani@live.com>
2018-07-28 21:15:44 -0500

    initial commit

---
+++ SpotifyClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpotifyClient
+//
+//  Created by Giorgi Khorguani on 7/28/18.
+//  Copyright © 2018 Giorgi Khorguani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
