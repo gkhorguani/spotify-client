e12e7e6cc9a1ec16adfffa65bc7ddbf811b08550
Giorgi Khorguani <gkhorguani@live.com>
2018-07-28 21:39:23 -0500

    rxswift and project structure

---
+++ SpotifyClient/View/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  SpotifyClient
+//
+//  Created by Giorgi Khorguani on 7/28/18.
+//  Copyright © 2018 Giorgi Khorguani. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Home vc..")
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
