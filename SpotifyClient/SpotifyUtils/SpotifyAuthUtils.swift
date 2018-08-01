e8b1f5f1bd7fe4adbabfea3fc8a4dee704a4b734
Giorgi Khorguani <gkhorguani@live.com>
2018-08-01 01:17:16 -0500

    refactor auth flow

---
+++ SpotifyClient/SpotifyUtils/SpotifyAuthUtils.swift
@@ -0,0 +1,35 @@
+//
+//  SpotifyAuthUtils.swift
+//  SpotifyClient
+//
+//  Created by Giorgi Khorguani on 8/1/18.
+//  Copyright © 2018 Giorgi Khorguani. All rights reserved.
+//
+
+import Foundation
+
+class SpotifyAuthUtils {
+    var clientId = ""
+    var redirectUrl = URL(string: "SpotifyClient://returnAfterLogin")
+    var loginUrl: URL?
+    let auth = SPTAuth.defaultInstance()
+    
+    init() {
+        // Try to read the env var
+        if let env_var_client_id = ProcessInfo.processInfo.environment["spotifyClientId"] {
+            clientId = env_var_client_id
+            
+            auth?.clientID = clientId
+            auth?.redirectURL = redirectUrl
+            auth?.requestedScopes = [SPTAuthStreamingScope]
+            
+            loginUrl = auth?.spotifyWebAuthenticationURL()
+        } else {
+            print("Could not read the environment variable - client id")
+        }
+    }
+    
+    func didMount() {
+        UIApplication.shared.open(loginUrl!, options: [:], completionHandler: nil)
+    }
+}
