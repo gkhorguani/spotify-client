ee1b1dfef12ca656b9f850ffa0527c69a2484454
Giorgi Khorguani <gkhorguani@live.com>
2018-08-01 00:56:24 -0500

    spotify sdk and login

---
+++ SpotifyClient/BridgeHeaders.h
@@ -0,0 +1,13 @@
+//
+//  BridgeHeaders.h
+//  SpotifyClient
+//
+//  Created by Giorgi Khorguani on 7/31/18.
+//  Copyright © 2018 Giorgi Khorguani. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BridgeHeaders : NSObject
+
+@end
