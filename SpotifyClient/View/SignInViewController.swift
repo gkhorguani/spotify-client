85a046b15b51aab4a4678e71eff5ca4030a1a814
Giorgi Khorguani <gkhorguani@live.com>
2018-08-05 22:05:38 -0500

    Player initializer - test play

--- SpotifyClient/View/SignInViewController.swift
+++ SpotifyClient/View/SignInViewController.swift
@@ -16,6 +16,7 @@ class SignInViewController: UIViewController {
     @IBOutlet weak var singInButton: UIButton!
     
     var spotifyAuthUtils: SpotifyAuthUtils?
+    var player: SPTAudioStreamingController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,6 +55,36 @@ extension SignInViewController: SpotifyLoginViewDelegate {
             let sessionDataObj = sessionObj as! Data
             let firstTimeSession = NSKeyedUnarchiver.unarchiveObject(with: sessionDataObj) as! SPTSession
             print(firstTimeSession)
+            
+            initializePlayer(authSession: firstTimeSession)
         }
+    }
+    
+    func initializePlayer(authSession:SPTSession){
+        if self.player == nil {
+            self.player = SPTAudioStreamingController.sharedInstance()
+            self.player!.playbackDelegate = self
+            self.player!.delegate = self
+            try! player!.start(withClientId: spotifyAuthUtils?.auth?.clientID)
+            self.player!.login(withAccessToken: authSession.accessToken)
+        }
+    }
+}
+
+extension SignInViewController: SPTAudioStreamingDelegate {
+    func audioStreamingDidLogin(_ audioStreaming: SPTAudioStreamingController!) {
+        // after a user authenticates a session, the SPTAudioStreamingController is then initialized and this method called
+        print("logged in")
+        self.player?.playSpotifyURI("spotify:track:7BHEi0cFs6TVNFDvbBrAjz", startingWith: 0, startingWithPosition: 0, callback: { (error) in
+            if (error != nil) {
+                print("\(String(describing: error))")
+            }
+        })
+    }
+}
+
+extension SignInViewController: SPTAudioStreamingPlaybackDelegate {
+    func audioStreaming(_ audioStreaming: SPTAudioStreamingController!, didStartPlayingTrack trackUri: String!) {
+        print("Started \(trackUri)")
     }
 }
