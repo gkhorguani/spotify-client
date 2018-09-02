083ec1fb91f8c90e5e25a3b2899f77e6de900d59
Giorgi Khorguani <gkhorguani@live.com>
2018-09-01 22:43:27 -0500

    Decouple home view controller from the view

---
+++ SpotifyClient/View/Home/HomeView.swift
@@ -0,0 +1,74 @@
+//
+//  HomeView.swift
+//  SpotifyClient
+//
+//  Created by Giorgi Khorguani on 9/1/18.
+//  Copyright © 2018 Giorgi Khorguani. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class HomeView: UIView {
+    
+    // MARK: - UI Elements
+    lazy var headerImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "header_texture")
+        iv.contentMode = .scaleToFill
+        
+        return iv
+    }()
+    
+    lazy var featuredPlaylistsCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let cv = UICollectionView(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: 100), collectionViewLayout: layout)
+        
+        cv.backgroundColor = .clear
+        
+        return cv
+    }()
+    
+    // MARK: - Render
+    func render() {
+        addSubview(headerImageView)
+        headerImageView.snp.makeConstraints { (make) in
+            make.leading.trailing.equalTo(self)
+            make.height.equalTo(150)
+        }
+        
+        let searchButton = UIButton()
+        searchButton.setTitle("Go", for: .normal)
+        searchButton.setTitleColor(.blue, for: .normal)
+        searchButton.layer.cornerRadius = 25
+        
+        let searchTextBox = UITextField()
+        searchTextBox.placeholder = "Search anything.."
+        searchTextBox.backgroundColor = .white
+        searchTextBox.layer.cornerRadius = 25
+        searchTextBox.setLeftPaddingPoints(20)
+        searchTextBox.rightView = searchButton
+        searchTextBox.rightViewMode = .always
+        addSubview(searchTextBox)
+        
+        searchButton.snp.makeConstraints { (make) in
+            make.width.height.equalTo(50)
+        }
+        
+        searchTextBox.snp.makeConstraints { (make) in
+            make.height.equalTo(50)
+            make.leading.equalTo(self).offset(50)
+            make.trailing.equalTo(self).offset(-50)
+            make.top.equalTo(self).offset(50)
+            make.centerX.equalTo(self)
+        }
+        
+        addSubview(featuredPlaylistsCollectionView)
+        featuredPlaylistsCollectionView.snp.makeConstraints { (make) in
+            make.top.equalTo(headerImageView.snp.bottom)
+            make.leading.trailing.equalTo(self)
+            make.height.equalTo(150)
+        }
+    }
+}
--- SpotifyClient/View/Home/HomeViewController.swift
+++ SpotifyClient/View/Home/HomeViewController.swift
@@ -7,38 +7,29 @@
 //
 
 import UIKit
-import SnapKit
 
-class HomeViewController: UIViewController, LayoutProvider, SideMenuable, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+class HomeViewController: UIViewController, LayoutProvider, SideMenuable {
     weak var sideMenuDelegate: SideMenuViewDelegate?
     var homeCoordinator: HomeCoordinator?
     var homePageVM = HomePageViewModel()
+    var homeView: HomeView! { return self.view as! HomeView }
     
-    // MARK: - UI Elements
-    lazy var headerImageView: UIImageView = {
-        let iv = UIImageView()
-        iv.image = UIImage(named: "header_texture")
-        iv.contentMode = .scaleToFill
-        
-        return iv
-    }()
+    private let cellid = "cellid"
     
-    lazy var featuredPlaylistsCollectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        let cv = UICollectionView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 100), collectionViewLayout: layout)
-        
-        cv.backgroundColor = .clear
-        
-        return cv
-    }()
+    override func loadView() {
+        view = HomeView(frame: UIScreen.main.bounds)
+        homeView.render()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
         setNavBar(title: "Home")
-        render()
         
+        homeView.featuredPlaylistsCollectionView.dataSource = self
+        homeView.featuredPlaylistsCollectionView.delegate = self
+        homeView.featuredPlaylistsCollectionView.register(FeaturedPlaylistsCell.self, forCellWithReuseIdentifier: cellid)
+
         homePageVM.getFeaturedPlaylists {
             print("Feched the playlists..")
         }
@@ -47,10 +38,6 @@ class HomeViewController: UIViewController, LayoutProvider, SideMenuable, UIColl
     
     func setupUI() {
         self.setupLayout()
-        
-        featuredPlaylistsCollectionView.dataSource = self
-        featuredPlaylistsCollectionView.delegate = self
-        featuredPlaylistsCollectionView.register(FeaturedPlaylistsCell.self, forCellWithReuseIdentifier: cellid)
     }
     
     @IBAction func onMenuTapped() {
@@ -62,57 +49,17 @@ class HomeViewController: UIViewController, LayoutProvider, SideMenuable, UIColl
         return navigationController
     }
     
-    // MARK: - Render
-    func render() {
-        view.addSubview(headerImageView)
-        headerImageView.snp.makeConstraints { (make) in
-            make.leading.trailing.equalTo(view)
-            make.height.equalTo(150)
-        }
-        
-        let searchButton = UIButton()
-        searchButton.setTitle("Go", for: .normal)
-        searchButton.setTitleColor(.blue, for: .normal)
-        searchButton.layer.cornerRadius = 25
-        
-        let searchTextBox = UITextField()
-        searchTextBox.placeholder = "Search anything.."
-        searchTextBox.backgroundColor = .white
-        searchTextBox.layer.cornerRadius = 25
-        searchTextBox.setLeftPaddingPoints(20)
-        searchTextBox.rightView = searchButton
-        searchTextBox.rightViewMode = .always
-        view.addSubview(searchTextBox)
-        
-        searchButton.snp.makeConstraints { (make) in
-            make.width.height.equalTo(50)
-        }
-        
-        searchTextBox.snp.makeConstraints { (make) in
-            make.height.equalTo(50)
-            make.leading.equalTo(view).offset(50)
-            make.trailing.equalTo(view).offset(-50)
-            make.top.equalTo(view).offset(50)
-            make.centerX.equalTo(view)
-        }
-        
-        view.addSubview(featuredPlaylistsCollectionView)
-        featuredPlaylistsCollectionView.snp.makeConstraints { (make) in
-            make.top.equalTo(headerImageView.snp.bottom)
-            make.leading.trailing.equalTo(view)
-            make.height.equalTo(150)
-        }
-    }
-    
+}
+
+extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     // MARK: - Featured playlists collection view
-    private let cellid = "cellid"
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 5
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return featuredPlaylistsCollectionView.dequeueReusableCell(withReuseIdentifier: cellid, for: indexPath) as! FeaturedPlaylistsCell
+        return homeView.featuredPlaylistsCollectionView.dequeueReusableCell(withReuseIdentifier: cellid, for: indexPath) as! FeaturedPlaylistsCell
     }
     
     func collectionView(_ collectionView: UICollectionView,
